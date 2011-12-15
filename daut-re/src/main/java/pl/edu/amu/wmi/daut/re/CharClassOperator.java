2fde8b366532e4f909105aef800aa2704453f9f1
Your Name <s362702@pc15b.labs.wmi.amu.edu.pl>
2011-12-15 10:39:01 +0100

    zmiana

--- daut-re/src/main/java/pl/edu/amu/wmi/daut/re/CharClassOperator.java
+++ daut-re/src/main/java/pl/edu/amu/wmi/daut/re/CharClassOperator.java
@@ -4,7 +4,7 @@
  */
 package pl.edu.amu.wmi.daut.re;
 
-import pl.edu.amu.wmi.daut.base.AutomatonSpecification;               
+import pl.edu.amu.wmi.daut.base.AutomatonSpecification;
 import pl.edu.amu.wmi.daut.base.NaiveAutomatonSpecification;
 import pl.edu.amu.wmi.daut.base.CharClassTransitionLabel;
 import pl.edu.amu.wmi.daut.base.State;
@@ -16,7 +16,7 @@ import pl.edu.amu.wmi.daut.base.State;
 public class CharClassOperator extends NullaryRegexpOperator {
     private String charClass;
 
-    
+
     public CharClassOperator(String Class) {
         charClass = Class;
     }
@@ -25,12 +25,12 @@ public class CharClassOperator extends NullaryRegexpOperator {
         AutomatonSpecification automaton = new NaiveAutomatonSpecification();
         State q0 = automaton.addState();
         State q1 = automaton.addState();
-        automaton.addTransition(q0, q1, new 
+        automaton.addTransition(q0, q1, new
 CharClassTransitionLabel(charClass));
         automaton.markAsInitial(q0);
         automaton.markAsFinal(q1);
 
         return automaton;
-    }        
+    }
 }
 
