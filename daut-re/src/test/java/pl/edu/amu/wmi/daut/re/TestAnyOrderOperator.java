a4d5a8e93b20dbf6d1392aa9a3abacd1f6bfbd39
Tomasz Morawski <tomasz.azrahel.morawski@gmail.com>
2011-12-18 23:29:26 +0100

    TestAnyOrderOperator.java dodane, poprawka1

--- daut-re/src/test/java/pl/edu/amu/wmi/daut/re/TestAnyOrderOperator.java
+++ daut-re/src/test/java/pl/edu/amu/wmi/daut/re/TestAnyOrderOperator.java
@@ -6,15 +6,15 @@ import pl.edu.amu.wmi.daut.base.State;
 import pl.edu.amu.wmi.daut.base.CharTransitionLabel;
 import junit.framework.TestCase;
 
-    /**
-    * Test klasy AnyOrderOperator.
-    */
+/**
+ * Test klasy AnyOrderOperator
+ */
 
 public class TestAnyOrderOperator extends TestCase {
 
     /**
-    * Test metody createAutomatonFromTwoAutomaton
-    **/
+     * Test metody createAutomatonFromTwoAutomaton
+     */
 
     public final void testCreateAutomatonFromTwoAutomata() {
 
@@ -22,7 +22,7 @@ public class TestAnyOrderOperator extends TestCase {
 
         State q0 = automaton1.addState();
         State q1 = automaton1.addState();
-        automaton1.addTransition(q0, q1, new CharTransitionLabel('ala'));
+        automaton1.addTransition(q0, q1, new CharTransitionLabel('a'));
 
         automaton1.markAsInitial(q0);
         automaton1.markAsFinal(q1);
@@ -33,20 +33,19 @@ public class TestAnyOrderOperator extends TestCase {
 
         State q2 = automaton2.addState();
         State q3 = automaton2.addState();
-        automaton2.addTransition(q2, q3, new CharTransitionLabel('kot'));
+        automaton2.addTransition(q2, q3, new CharTransitionLabel('k'));
 
         automaton2.markAsInitial(q2);
         automaton2.markAsFinal(q3);
 
         AnyOrderOperator operator = new AnyOrderOperator();
         AutomatonSpecification result =
-        new NaiveAutomatonSpecification(
-                operator.createAutomatonFromTwoAutomata(automaton1, automaton2));
+        new NaiveAutomatonSpecification(operator.createAutomatonFromTwoAutomata(automaton1, automaton2));
 
-        assertTrue(result.accepts("alakot"));
-        assertTrue(result.accepts("kotala"));
-        assertFalse(result.accepts("kot"));
-        assertFalse(result.accepts("ala"));
+        assertTrue(result.accepts("ak"));
+        assertTrue(result.accepts("ka"));
+        assertFalse(result.accepts("a"));
+        assertFalse(result.accepts("k"));
         assertFalse(result.accepts(""));
     }
 }
