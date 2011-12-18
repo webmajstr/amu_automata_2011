f63ae60f1ef635a8d88919976df28c01dfb86a1e
Tomasz Morawski <tomasz.azrahel.morawski@gmail.com>
2011-12-18 21:16:25 +0100

    TestAnyOrderOperator.java dodane

---
+++ daut-re/src/test/java/pl/edu/amu/wmi/daut/re/TestAnyOrderOperator.java
@@ -0,0 +1,53 @@
+package pl.edu.amu.wmi.daut.re;
+
+import pl.edu.amu.wmi.daut.base.AutomatonSpecification;
+import pl.edu.amu.wmi.daut.base.NaiveAutomatonSpecification;
+import pl.edu.amu.wmi.daut.base.State;
+import pl.edu.amu.wmi.daut.base.CharTransitionLabel;
+import junit.framework.TestCase;
+
+    /**
+    * Test klasy AnyOrderOperator.
+    */
+
+public class TestAnyOrderOperator extends TestCase {
+
+    /**
+    * Test metody createAutomatonFromOneAutomaton
+    * z automatem z jednym stanem.
+    */
+
+    public final void testCreateAutomatonFromTwoAutomata() {
+
+        AutomatonSpecification automaton1 = new NaiveAutomatonSpecification();
+
+        State q0 = automaton1.addState();
+        State q1 = automaton1.addState();
+        automaton1.addTransition(q0, q1, new CharTransitionLabel('ala'));
+
+        automaton1.markAsInitial(q0);
+        automaton1.markAsFinal(q1);
+
+
+
+        AutomatonSpecification automaton2 = new NaiveAutomatonSpecification();
+
+        State q2 = automaton2.addState();
+        State q3 = automaton2.addState();
+        automaton2.addTransition(q2, q3, new CharTransitionLabel('kot'));
+
+        automaton2.markAsInitial(q2);
+        automaton2.markAsFinal(q3);
+
+        AnyOrderOperator operator = new AnyOrderOperator();
+        AutomatonSpecification result =
+        new NaiveAutomatonSpecification(
+                operator.createAutomatonFromTwoAutomata(automaton1, automaton2));
+
+        assertTrue(result.accepts("alakot"));
+        assertTrue(result.accepts("kotala"));
+        assertFalse(result.accepts("kot"));
+        assertFalse(result.accepts("ala"));
+        assertFalse(result.accepts(""));
+    }
+}
