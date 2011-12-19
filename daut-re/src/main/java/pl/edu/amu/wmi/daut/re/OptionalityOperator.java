65cca2c686bbb111332c401e782e2d9e7f943863
Natalia Malinowska <nm76006@st.amu.edu.pl>
2011-12-19 23:31:44 +0100

    poprawiona metoda

--- daut-re/src/main/java/pl/edu/amu/wmi/daut/re/OptionalityOperator.java
+++ daut-re/src/main/java/pl/edu/amu/wmi/daut/re/OptionalityOperator.java
@@ -2,9 +2,9 @@ package pl.edu.amu.wmi.daut.re;
 import pl.edu.amu.wmi.daut.base.AutomatonSpecification;
 import pl.edu.amu.wmi.daut.base.EpsilonTransitionLabel;
 import pl.edu.amu.wmi.daut.base.State;
+import java.util.List;
 
 /**
- *
  * Klasa reprezentujaca operator ?
  */
 public class OptionalityOperator extends UnaryRegexpOperator {
@@ -15,10 +15,25 @@ public class OptionalityOperator extends UnaryRegexpOperator {
     public AutomatonSpecification createAutomatonFromOneAutomaton(
             AutomatonSpecification subautomaton) {
         State q0 = subautomaton.addState();
-        subautomaton.markAsInitial(q0);
-        subautomaton.markAsFinal(q0);
         subautomaton.addTransition(q0, subautomaton.getInitialState(),
             new EpsilonTransitionLabel());
+        subautomaton.markAsInitial(q0);
+        subautomaton.markAsFinal(q0);
         return subautomaton;
+    }
+
+     /**
+      * Fabryka operatora.
+      */
+    public static class Factory extends UnaryRegexpOperatorFactory {
+
+        @Override
+        public int numberOfParams() {
+            return 0;
+        }
+
+        protected RegexpOperator doCreateOperator(List<String> params) {
+            return new OptionalityOperator();
+        }
     }
 }
