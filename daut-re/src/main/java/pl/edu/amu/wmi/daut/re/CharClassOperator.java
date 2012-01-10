fc4007ef0d244c7ea5154cb0bd04752818eb29be
unknown <Damian@Damian-Komputer.(none)>
2012-01-10 22:55:16 +0100

    zmiany

---
+++ daut-re/src/main/java/pl/edu/amu/wmi/daut/re/CharClassOperator.java
@@ -0,0 +1,29 @@
+package pl.edu.amu.wmi.daut.re;
+
+import pl.edu.amu.wmi.daut.base.AutomatonSpecification;
+import pl.edu.amu.wmi.daut.base.NaiveAutomatonSpecification;
+import pl.edu.amu.wmi.daut.base.CharClassTransitionLabel;
+import pl.edu.amu.wmi.daut.base.State;
+
+public class CharClassOperator extends NullaryRegexpOperator {
+    private String charClass;
+
+    /**
+    * Konstruktor, pobiera porzadana klase znakow.
+    **/
+    public CharClassOperator(String Class) {
+        charClass = Class;
+    }
+    @Override
+    public AutomatonSpecification createFixedAutomaton() {
+        AutomatonSpecification automaton = new NaiveAutomatonSpecification();
+        State q0 = automaton.addState();
+        State q1 = automaton.addState();
+        automaton.addTransition(q0, q1, new
+CharClassTransitionLabel(charClass));
+        automaton.markAsInitial(q0);
+        automaton.markAsFinal(q1);
+
+        return automaton;
+    }
+}
