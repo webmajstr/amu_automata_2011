8799831bb0d233610b2c0edaf8152bea90d2db45
Tomasz Morawski <tomasz.azrahel.morawski@gmail.com>
2012-01-14 15:03:45 +0100

    koncowe poprawki

--- daut-re/src/test/java/pl/edu/amu/wmi/daut/re/TestAnyOrderOperator.java
+++ daut-re/src/test/java/pl/edu/amu/wmi/daut/re/TestAnyOrderOperator.java
@@ -9,32 +9,31 @@ import junit.framework.TestCase;
 /**
  * Test klasy AnyOrderOperator
  */
-
 public class TestAnyOrderOperator extends TestCase {
-
-    /**
+	
+	/**
+     * Test konstruktora AnyOrderOperator
+     */
+	public void testAnyOrderOperator() {
+        AnyOrderOperator operator = new AnyOrderOperator();
+    }
+	
+	/**
      * Test metody createAutomatonFromTwoAutomaton
      */
-
     public final void testCreateAutomatonFromTwoAutomata() {
 
         AutomatonSpecification automaton1 = new NaiveAutomatonSpecification();
-
         State q0 = automaton1.addState();
         State q1 = automaton1.addState();
         automaton1.addTransition(q0, q1, new CharTransitionLabel('a'));
-
         automaton1.markAsInitial(q0);
         automaton1.markAsFinal(q1);
 
-
-
         AutomatonSpecification automaton2 = new NaiveAutomatonSpecification();
-
         State q2 = automaton2.addState();
         State q3 = automaton2.addState();
         automaton2.addTransition(q2, q3, new CharTransitionLabel('k'));
-
         automaton2.markAsInitial(q2);
         automaton2.markAsFinal(q3);
 
@@ -46,5 +45,34 @@ public class TestAnyOrderOperator extends TestCase {
         assertFalse(result.accepts("a"));
         assertFalse(result.accepts("k"));
         assertFalse(result.accepts(""));
+
+        AutomatonSpecifitacion automaton3 = new NaiveAutomatonSpecification();
+        State q4 = automaton3.addState();
+        State q5 = automaton3.addState();
+        automaton3.addTransition(q4, q5, new CharTransitionLabel(''));
+        automaton3.markAsInitial(q4);
+        automaton3.markAsFinal(q5);
+
+        AutomatonSpecification automaton4 = new NaiveAutomatonSpecification();
+        State q6 = automaton2.addState();
+        State q7 = automaton2.addState();
+        automaton2.addTransition(q6, q7, new CharTransitionLabel(''));
+        automaton2.markAsInitial(q6);
+        automaton2.markAsFinal(q7);        
+
+        AnyOrderOperator operator1 = new AnyOrderOperator();
+        AutomatonSpecification result1 = operator1.createAutomatonFromTwoAutomata(automaton3, automaton4);
+        
+        assertTrue(result1.accepts(""));   
+    }
+
+    /**
+     * Test fabryki
+     */
+    public final void testFactory() {
+        Factory factory = new Factory();
+        ArrayList<String> params = new ArrayList<String>();
+        assertEquals(factory.createOperator(params).getClass(),
+                new AnyOrderOperator().getClass());
     }
 }
