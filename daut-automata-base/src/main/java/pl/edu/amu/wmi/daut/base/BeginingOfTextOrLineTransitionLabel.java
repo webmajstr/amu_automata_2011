eabdc118b676703b022340d445a12816668d4bd1
Grzegorz Bednarski <gb94722@st.amu.edu.pl>
2012-01-16 19:38:22 +0100

    Issue #493 rozwiazanie

---
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/BeginingOfTextOrLineTransitionLabel.java
@@ -0,0 +1,32 @@
+package pl.edu.amu.wmi.daut.base;
+
+/**
+ * Klasa BeginingOfTextOrLineTransitionLabel, która odpowiada operatorowi "^" z
+ * z wyrażeń regularnych standardu POSIX.
+ */
+public class BeginingOfTextOrLineTransitionLabel
+    extends ZeroLengthConditionalTransitionLabel {
+
+    @Override
+    protected boolean doCheckContext(String s, int position) {
+        if (!s.isEmpty() && position == 0 || s.charAt(-1) == '\n') {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean canAcceptCharacter(char c) {
+        return false;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "BeginingOfTextOrLine";
+    }
+}
