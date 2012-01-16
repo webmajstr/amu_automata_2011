c100e026be8010919a7f11a2d89fa768289ef6b0
irmina90 <weronika.zietek@gmail.com>
2012-01-16 23:50:08 +0100

    nawiasy

--- daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/EndOfTextTransitionLabel.java
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/EndOfTextTransitionLabel.java
@@ -11,7 +11,7 @@ public class EndOfTextTransitionLabel extends ZeroLengthConditionalTransitionLab
 
     @Override
     protected boolean doCheckContext(String s, int position) {
-        if ((position < 0) || (position > s.length())) {
+        if (position < 0 || position > s.length()) {
             throw new PositionOutOfStringBordersException();
         }
         if (position == s.length()) {
