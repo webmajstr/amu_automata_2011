0dab296f5960ae4f3ec3def9f1a8ba142bf66add
none <none>
2012-01-23 13:33:49 +0100

    Fabryka do CharClassOperator

--- daut-re/src/main/java/pl/edu/amu/wmi/daut/re/CharClassOperator.java
+++ daut-re/src/main/java/pl/edu/amu/wmi/daut/re/CharClassOperator.java
@@ -1,6 +1,7 @@
 package pl.edu.amu.wmi.daut.re;
 
-import pl.edu.amu.wmi.daut.base.AutomatonSpecification;
+import java.util.List;
+import pl.edu.amu.wmi.daut.base.AutomatonSpecification;               
 import pl.edu.amu.wmi.daut.base.NaiveAutomatonSpecification;
 import pl.edu.amu.wmi.daut.base.CharClassTransitionLabel;
 import pl.edu.amu.wmi.daut.base.State;
@@ -9,8 +10,8 @@ public class CharClassOperator extends NullaryRegexpOperator {
     private String charClass;
 
     /**
-    * Konstruktor, pobiera porzadana klase znakow.
-    */
+     * Konstruktor, pobiera pożądaną klasę znaków.
+     */
     public CharClassOperator(String c) {
         this.charClass = c;
     }
@@ -19,11 +20,34 @@ public class CharClassOperator extends NullaryRegexpOperator {
         AutomatonSpecification automaton = new NaiveAutomatonSpecification();
         State q0 = automaton.addState();
         State q1 = automaton.addState();
-        automaton.addTransition(q0, q1, new
-CharClassTransitionLabel(charClass));
+        automaton.addTransition(q0, q1,
+		    new CharClassTransitionLabel(charClass));
         automaton.markAsInitial(q0);
         automaton.markAsFinal(q1);
 
         return automaton;
+    }        
+    
+     /**
+      * Fabryka operatora.
+      */
+    public static class Factory extends NullaryRegexpOperatorFactory {
+
+        @Override
+        public int numberOfParams() {
+            return 1;
+        }
+
+        protected RegexpOperator doCreateOperator(List<String> params) {
+            return new CharClassOperator(params.get(0));
+        }
+     }
+    
+    /**
+     * Metoda toString().
+     */
+    @Override
+    public String toString() {
+        return "CHAR_CLASS_" + charClass;
     }
 }
