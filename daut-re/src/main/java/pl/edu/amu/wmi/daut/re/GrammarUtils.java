64b39a378af2600cd5acc80d7e4170b7eea4e400
Qellq <m.gaik7@gmail.com>
2012-01-23 17:23:51 +0100

    poprawa zadania #563

--- daut-re/src/main/java/pl/edu/amu/wmi/daut/re/GrammarUtils.java
+++ daut-re/src/main/java/pl/edu/amu/wmi/daut/re/GrammarUtils.java
@@ -8,15 +8,19 @@ import java.util.Vector;
  * metoda sprawdzająca, czy podana gramatyka jest gramatyką liniową
  */
 public class GrammarUtils {
-   public boolean isLinear(Grammar g) {
-      for (GrammarRule rule : g.allRules()) {
+    public boolean isLinear(Grammar g) {
+        for (GrammarRule rule : g.allRules()) {
           
-         if (!rule.getRhsFirstSymbol().isTerminalSymbol() && !rule.getRhsSecondSymbol().isTerminalSymbol()) {
+            int terminalSymbols = 0;
+         
+            for(GrammarSymbol symbol : rule.getRhsSymbols()) 
+                if(symbol.isTerminalSymbol())
+                    terminalSymbols++;
+         
+        if(terminalSymbols > 1)
             return false;
-            
-         }
-      }
-      return true;
-   }
+        }
+    return true;
+    }
 }
 
