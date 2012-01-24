79cb7c559ccd8716d832e2faf324cbcda0b2cfb3
Qellq <m.gaik7@gmail.com>
2012-01-24 15:04:05 +0100

    zadanie #563

--- daut-re/src/main/java/pl/edu/amu/wmi/daut/re/GrammarUtils.java
+++ daut-re/src/main/java/pl/edu/amu/wmi/daut/re/GrammarUtils.java
@@ -1,19 +1,22 @@
 package pl.edu.amu.wmi.daut.re;
 
 /**
- * metoda sprawdzająca, czy podana gramatyka jest gramatyką liniową
+ * metoda sprawdzająca, czy podana gramatyka jest gramatyką liniową.
  */
 public class GrammarUtils {
+    /**
+     * Główna metoda klasy.
+     */
     public boolean isLinear(Grammar g) {
         for (GrammarRule rule : g.allRules()) {
-          
+
             int terminalSymbols = 0;
-         
-                for(GrammarSymbol symbol : rule.getRhsSymbols())
-                    if(symbol.isTerminalSymbol())
+
+                for (GrammarSymbol symbol : rule.getRhsSymbols())
+                    if (symbol.isTerminalSymbol())
                         terminalSymbols++;
-          
-            if(terminalSymbols > 1)
+
+            if (terminalSymbols > 1)
                 return false;
         }
         return true;
